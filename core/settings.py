86bbd3510855e8ca882897094651a2b6c3ff97b3
Anish Kumal <anish.kumal.a23@icp.edu.np>
2026-04-16 16:14:34 +0545

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -166,9 +166,13 @@ CHANNEL_LAYERS = {
 
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': config('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
